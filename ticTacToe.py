6f0518c6a0a9ba2c0933788980be8a4e3b9bf1da
dlbjames <51972606+dlbjames@users.noreply.github.com>
2020-07-15 09:40:01 -0700

    Added AI player
    Albeit far from perfect - added an AI for the player to play against.
    Used a modified form of the minimax algorithm to achieve an AI that is able to be beaten, but hard enough to keep the player on guard (hopefully).

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,100 +1,180 @@
-def main():
-    while True:
-        Play()
-        again = input("Do you want to play again? (y / n): ")
-        if (again != "y"):
-            break
+# Creates the board for us to play on
+# and updates the board as the players take
+# their turns.
+def play():
+    board = [[1, 2, 3],
+             [4, 5, 6],
+             [7, 8, 9]]
 
-
-def Play():
-    board = [[1], [2], [3],
-             [4], [5], [6],
-             [7], [8], [9]
-             ]
     displayBoard(board)
-
-    player = True
+    player = 'X'
     count = 0
-    win = False
 
     while count != 9:
-        ans = int(input("Mark an untaken spot: "))
-        while taken(board, ans, player):
+        # AI's Move
+        if count % 2:
+            (maxMove, x, y) = getMax(board, player)
+            print("AI's Move: {}, {}".format(x, y))
+            print()
+            
+            # Calculates the position on the board (1, 2, 3, 4, 5, ..., 9)
+            taken(board, (3 * x) + y + 1, player)
+
+        #Player's Move
+        else:
+            (minMove, minX, minY) = getMin(board, player)
+            print("Recommended Move: {}".format((3 * minX) + minY + 1))
+            print()
             ans = int(input("Mark an untaken spot: "))
+            while taken(board, ans, player):
+                ans = int(input("Mark an untaken spot: "))
 
         displayBoard(board)
         count += 1
 
-        if (playerWon(board, player)):
+        if playerWon(board, player) == player:
             break
-
-        if player:
-           player = False
+        # Change the player's turns
+        if player == 'X':
+           player = 'O'
         else:
-            player = True
+            player = 'X'
 
-    if count == 9 and not playerWon(board, player):
+    if count == 9 and playerWon(board, player) != player:
         print("Its a Draw!")
-    elif player:
-        print("Player 1 Won!")
+    elif 'X' == playerWon(board, player):
+        print("You beat the AI!")
     else:
-        print("Player 2 Won!")
+        print("The AI Won!")
 
-
+# Determines if a spot on the board
+# is already taken. 
 def taken(board, spot, player):
-    if board[spot -1][0] != spot:
+    if board[(spot - 1) // 3][spot % 3 - 1] != spot:
         return True
-    if player:
-        board[spot - 1][0] = 'X'
+    if player == 'X':
+        board[(spot - 1) // 3][spot % 3 - 1] = 'X'
     else:
-        board[spot - 1] = 'O'
-
+        board[(spot - 1) // 3][spot % 3 - 1] = 'O'
     return False 
 
-
+# Displays the board to the console.
 def displayBoard(board):
     print()
     for i in range(len(board)):
         print("| ", end="")
-        print(board[i][0], end=" ")
-        if ( (i + 1) % 3 == 0 and (i + 1) != 9):
-            print("|")
+        for j in range(len(board[0])):
+            print(board[i][j], end=" ")
+            print("|", end=" ")
+
+        print()
+        if i != 2:
             print("|___|___|___|")
-    print("|")
+        else:
+            print("|   |   |   |")
     print()
 
-
+# Determines if a player has won the game.
 def playerWon(board, player):
-    letter = ''
-    if player:
-        letter = 'X'
-    else:
-        letter = 'O'
+    # Horizontal Win
+    for i in range(0, 3):
+        if [player, player, player] == board[i]:
+            return player
+
+    # Vertical Win
+    for i in range(0, 3):
+        if board[0][i] == player and board[0][i] == board[1][i] and board[1][i] == board[2][i]:
+            return player
+
+    # Top Left Diagonal Win
+    if board[0][0] == player and board[0][0] == board[1][1] and board[0][0] == board[2][2]:
+        return player
+
+    # Top Right Diagonal Win
+    if board[0][2] == player and board[0][2] == board[1][1] and board[0][2] == board[2][0]:
+        return player
+
+    # Check if there are still empty spots
+    for i in range(0, 3):
+        for j in range(0, 3):
+            if board[i][j] != 'X' and board[i][j] != 'O':
+                return None
     
-    if letter == board[0][0] and letter == board[3][0] and letter == board[6][0]:
-        return True
+    return '.'
+
+# Gets the computer's move that causes
+# the 'greatest' chance of winning.
+def getMax(board, player):
+    maxMove = -2
+    px = None
+    py = None
     
-    if letter == board[0][0] and letter == board[1][0] and letter == board[2][0]:
-        return True
+    result = playerWon(board, player)
 
-    if letter == board[0][0] and letter == board[4][0] and letter == board[8][0]:
-        return True
+    if result == 'X':
+        return (-1, 0, 0)
+    elif result == 'O':
+        return (1, 0, 0)
+    elif result == '.':
+        return (0, 0, 0)
 
-    if letter == board[1][0] and letter == board[4][0] and letter == board[7][0]:
-        return True
+    # For each possible move left
+    # Check if that move is a 'good move'.
+    for i in range(0, 3):
+        for j in range(0, 3):
+            if board[i][j] != 'X' and board[i][j] != 'O':
+                temp = board[i][j]
+                board[i][j] = 'O'
+                (minMove, min_i, min_j) = getMin(board, player)
+                if minMove > maxMove:
+                    maxMove = minMove
+                    px = i
+                    py = j
+                board[i][j] = temp
+    return (maxMove, px, py) 
 
-    if letter == board[2][0] and letter == board[4][0] and letter == board[6][0]:
-        return True
+# Gets the computer's move that 
+# causes the 'least' chance of a loss.
+def getMin(board, player):
+    minMove = 2
+    qx = None
+    qy = None
     
-    if letter == board[2][0] and letter == board[5][0] and letter == board[8][0]:
-        return True
+    # Result is the player variable
+    result = playerWon(board, player)
 
-    if letter == board[3][0] and letter == board[4][0] and letter == board[5][0]:
-        return True
+    if result == 'X':
+        return (-1, 0, 0)
+    elif result == 'O':
+        return (1, 0, 0)
+    elif result == '.':
+        return (0, 0, 0)
 
-    if letter == board[6][0] and letter == board[7][0] and letter == board[8][0]:
-        return True
-    
-    return False
+    # For each possible move left
+    # Check if that move is a 'good move'.
+    for i in range(0, 3):
+        for j in range(0, 3):
+            if board[i][j] != 'X' and board[i][j] != 'O':
+                temp = board[i][j]
+                board[i][j] = 'X'
+                (maxMove, max_i, max_j) = getMax(board, player)
 
-main()
+                if maxMove < minMove:
+                    minMove = maxMove
+                    qx = i
+                    qy = j
+                board[i][j] = temp
+    return (minMove, qx, qy) 
+
+# The main game loop that allows us
+# to play the game asking for input 
+# when the game is finished.
+def main():
+    while True:
+        play()
+        again = input("Do you want to play again? (y / n): ")
+        if (again != "y"):
+            break
+
+if __name__ == "__main__":
+    main()
